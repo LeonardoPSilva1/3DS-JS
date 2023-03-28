41ec9c77468ffb9bd3cf6d1b1faf173fa7f26ab3
Leonardo Pereira da Silva <leonardop.silva1622@gmail.com>
2023-03-27 22:24:25 -0300

    EVENTOS JS

---
+++ js/evento no js/js/script.js
@@ -0,0 +1,10 @@
+ var a = 0;
+ var c = 0;
+
+ function down(){
+    document.getElementById("eventoKeyDown").innerHTML = a+=1;
+ }
+
+ function up(){
+    document.getElementById("eventoKeyUp").innerHTML = c+=1;
+ }
